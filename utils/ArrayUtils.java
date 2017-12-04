5c635c0da014ed2081784b39ece93d11095eb97b
AnahitMan <31693804+AnahitMan@users.noreply.github.com>
2017-12-04 15:58:53 +0400

    Update ArrayUtils.java

--- utils/ArrayUtils.java
+++ utils/ArrayUtils.java
@@ -9,7 +9,7 @@ public class ArrayUtils {
 	}
 	public static String stringifyArray(int[] arr){
 		String result = "" + arr[0];
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 1; i < arr.length; i++) {
 			result = result + "," + arr[i];
 		}
 		return result;
